24318fac382daca5993d52b9dcb1bce559545770
root <root@ip-172-31-32-82.us-east-2.compute.internal>
2023-08-22 11:30:22 +0000

    first commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+        return "Hello, this is created by Nisha!"
+
+    if __name__ == "__main__":
+            app.run(host='0.0.0.0', port=5000, debug=True, use_reloader=True)from flask import Flask
+            app = Flask(__name__)
+
+            @app.route("/")
+            def home():
+                    return "Hello, this is created by Nisha!"
+
+                if __name__ == "__main__":
+                        app.run(host='0.0.0.0', port=5000, debug=True, use_reloader=True)
